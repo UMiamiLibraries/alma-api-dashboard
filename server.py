8982145975271e9af6aedaa63c03b451ba1fe025
Eddie <eprieto.n@gmail.com>
2019-09-09 21:37:19 -0400

    new functionality for retrieving set results new home page remove error pages

--- server.py
+++ server.py
@@ -6,25 +6,25 @@ import config
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  return render_template('form.html')
+    return render_template('home.html')
+
 
 @app.route('/submit-new-user/', methods=['POST'])
-def SubmitNewUser():
-    print("*******creating new user*******")
-	
-    #get data from flask request object
+def submitnewuser():
+    # get data from flask request object
     fname = request.form['fname']
     lname = request.form['lname']
     email = request.form['email']
     u_username = request.form['primary_id']
     u_password = request.form['password']
 
-    #define user xml object
+    # define user xml object
     user_xml = etree.fromstring('<?xml version="1.0"?><user><record_type desc="Public">PUBLIC</record_type><primary_id></primary_id><first_name></first_name><last_name></last_name><full_name></full_name><user_group desc="Special Collections">SPECCOLL</user_group><preferred_language desc="English">en</preferred_language><account_type desc="Internal">INTERNAL</account_type><external_id></external_id><password></password><force_password_change>true</force_password_change><status desc="Active">ACTIVE</status><contact_info><emails><email preferred="true" segment_type="Internal"><email_address></email_address><email_types><email_type desc="personal">personal</email_type></email_types></email></emails></contact_info><user_roles><user_role><status desc="Active">ACTIVE</status><scope desc="University of Miami">01UOML_INST</scope><role_type desc="Patron">200</role_type></user_role></user_roles></user>')
     
-    #inject user data into user xml object
+    # inject user data into user xml object
     primary_id = user_xml.findall(".//primary_id")
     primary_id[0].text = u_username
     first_name = user_xml.findall(".//first_name")
@@ -38,49 +38,68 @@ def SubmitNewUser():
     password = user_xml.findall(".//password")
     password[0].text = u_password
    
-    #define request parameters
+    # define request parameters
     url = 'https://api-eu.hosted.exlibrisgroup.com/almaws/v1/users?social_authentication=false&send_pin_number_letter=false'
     user = etree.tostring(user_xml)
     print(user)
     headers = {'Content-Type': 'application/xml'}
     payload = {'apikey' : config.apikey}
     
-    #create request url
+    # create request url
     response = requests.post(url, headers=headers, params=payload, data=user)
-    print(response.url)
-    #print("***" + response.url + "***")
     root = etree.fromstring(response.content)
     soup = str(BeautifulSoup(etree.tostring(root), 'lxml'))
-    print(soup)
     names = root.findall(".//full_name")
     try:
 	    name = names[0].text
     except IndexError:
         return render_template("error_create.html", full_name=full_name)
     return render_template("user_create.html", name=name)
-    
-@app.route('/lookup-user/', methods=['POST'])
-def LookupUser():
-    print("*******searching for user*******")
-    userid = request.form['userid']
 
-    #create search parameters
-    payload = {'apikey' : config.apikey,}
-    url = 'https://api-eu.hosted.exlibrisgroup.com/almaws/v1/users/' + userid + '?'
-    response = requests.get(url, params=payload)
-    #print("***" + response.url + "***")
-    root = etree.fromstring(response.content)
-    soup = str(BeautifulSoup(etree.tostring(root), 'lxml'))
-    names = root.findall(".//full_name")
-    emails = root.findall(".//email_address")
-    name = ""
-    email = ""
-    try:
-        name = names[0].text
-        email = emails[0].text
-    except IndexError:
-        return render_template("error_search.html", userid=userid)
-    return render_template("user_search.html", name=name, email=email)
-	
+
+@app.route('/users/', methods=['GET', 'POST'])
+def lookupuser():
+    if request.method == 'GET':
+        return render_template('users.html')
+    else:
+        print("*******searching for user*******")
+        userid = request.form['userid']
+
+        #create search parameters
+        payload = {'apikey' : config.apikey}
+        url = 'https://api-eu.hosted.exlibrisgroup.com/almaws/v1/users/' + userid + '?'
+        response = requests.get(url, params=payload)
+        root = etree.fromstring(response.content)
+        names = root.findall(".//full_name")
+        emails = root.findall(".//email_address")
+        try:
+            name = names[0].text
+            email = emails[0].text
+        except IndexError:
+            return render_template("user_search.html", userid=userid, error="true")
+        return render_template("user_search.html", name=name, email=email, error="false")
+
+
+@app.route('/sets/', methods=['GET', 'POST'])
+def getsetcontents():
+    if request.method == 'GET':
+        return render_template("sets.html")
+    else:
+        setid = request.form['setid']
+        payload = {'apikey': config.apikey}
+        url = 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/conf/sets/' + setid + '/members?'
+        response = requests.get(url, params=payload)
+        root = etree.fromstring(response.content)
+        memberids = root.findall(".//member")
+        setmembers = []
+        for member in memberids:
+            response = requests.get(member.attrib['link'], params=payload)
+            root = etree.fromstring(response.content)
+            setmember = [root.find(".//title").text, root.find(".//copy_id").text, root.find(".//temp_location").text]
+            setmembers.append(setmember)
+        return render_template("set_results.html", setmembers=setmembers)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
